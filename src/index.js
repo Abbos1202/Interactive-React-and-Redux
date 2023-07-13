460d94daf685518982d641c9c087fac1909a458e
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-07-13 12:53:56 +0500

    table

--- src/index.js
+++ src/index.js
@@ -1,65 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const color = "blue"
+const cars = [
+  {
+    id: 1,
+    carName: "Malibu",
+    carPrice: 30000,
+  },
+  {
+    id: 2,
+    carName: "Gentra",
+    carPrice: 25000,
+  },
+  {
+    id: 3,
+    carName: "Tracker",
+    carPrice: 28000,
+  },
+  {
+    id: 4,
+    carName: "Nexia 3",
+    carPrice: 10000,
+  },
+  {
+    id: 5,
+    carName: "Equinox",
+    carPrice: 35000,
+  },
+  {
+    id: 6,
+    carName: "Captiva",
+    carPrice: 28000,
+  },
+];
 
 ReactDOM.render(
   <div>
-    <h1 style={{color: "red", background: "yellow"}}>Welcome to our hub!</h1>
-    <p style={{color: color, background: "coral"}}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste provident
-      alias explicabo consequatur. Sint, possimus!
-    </p>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <table className="table table-light table-hover">
+            <thead className="table-dark">
+              <tr>
+                <th>#</th>
+                <th>Car Name</th>
+                <th>Car Price</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {cars.map((value) => {
+                return (
+                  <tr key={value.id}>
+                    <td>{value.id}</td>
+                    <td>{value.carName}</td>
+                    <td>{value.carPrice}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>,
   document.getElementById("root")
 );
-
-// let numbers = [4, 5, 6]
-
-// ReactDOM.render(
-//     <div>
-//         <p>{numbers}</p>
-//         <p>{numbers.join(" , ")}</p>
-//         <p>{numbers.map(number => number + (" / "))}</p>
-//     </div>,
-//     document.getElementById("root")
-// )
-
-// let number = 4;
-
-// ReactDOM.render(
-//     <div>{number * 10}</div>,
-//     document.getElementById("root")
-// )
-
-// const user = {
-//     name: "Angela",
-//     city: "California",
-// }
-
-// ReactDOM.render(
-//     <div>
-//         <h2>Name: {user.name}</h2>
-//         <h2>City: {user.city}</h2>
-//     </div>
-//     ,document.getElementById("root"))
-
-// const firstName = "Abbos";
-// const lastName = "Ibrohimov";
-
-// ReactDOM.render(
-//   <div className="container">
-//     <p><b>Name:</b> {firstName}</p>
-//     <p><b>Surname:</b> {lastName}</p>
-//   </div>,
-//   document.getElementById("root")
-// );
-
-// ReactDOM.render(
-//     React.createElement("div", {className: "container"},
-//     React.createElement("h1", {}, "Qalaysan"),
-//     React.createElement("div", {className: "box"},
-//     React.createElement("p", {}, "Yaxshi o'zing yaxshimisan"))),
-//     document.getElementById("root")
-
-// )
