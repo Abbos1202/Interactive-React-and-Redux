b86fdbacd179fd8ec79e32e2b402fbdab99fc78a
Abbos1202 <125945188+Abbos1202@users.noreply.github.com>
2023-07-14 06:14:22 +0500

    style.js

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
-  height: 450px;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
+  position: absolute;
+  text-align: center;
+  bottom: 0;
 `;
 
 
