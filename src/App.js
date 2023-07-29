8475c164f383f08dabb6068a0fdb75345942ae69
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-07-29 15:22:00 +0500

    ref

--- src/App.js
+++ src/App.js
@@ -1,88 +1,116 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export class App extends Component {
   state = {
-    email: "",
-    isAgree: false,
+    cardNumber: "",
+    cardCvv: "",
+    cardName: "",
   };
 
-  handleEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
+  cardNumberRef = createRef();
+  cardCvvRef = createRef();
+  cardNameRef = createRef();
+
+  handleValue = (e) => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value,
+      },
+      () => {
+        if (this.state.cardNumber.length === 16) {
+          this.cardCvvRef.current.focus();
+        }
+        if (this.state.cardCvv.length === 3) {
+          this.cardNameRef.current.focus();
+        }
+      }
+    );
   };
 
-  handleChecking = (e) => {
-    this.setState({
-      isAgree: e.target.checked,
-    });
-  };
-
-  submitBtn = () => {
-    const regEmail =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const isValidEmail = regEmail.test(this.state.email);
-
-    const isValidIsActive = this.state.isAgree;
-
-    if (!isValidEmail) {
-      alert("Please, enter valid email!");
-      return;
-    }
-
-    if (!isValidIsActive) {
-      alert("Please, agree with terms!");
-      return;
-    }
-
-    this.setState({
-      email: "",
-      isAgree: false,
-    });
-
-    alert("Congratulations 🎉");
-  };
+  componentDidMount() {
+    this.cardNumberRef.current.focus();
+  }
 
   render() {
-    const { email, isAgree } = this.state;
+    const { cardNumber, cardCvv, cardName } = this.state;
     return (
-      <div className="contact">
-        <div className="contact-card">
-          <div className="contact-title">
-            <h2>Contact Us</h2>
+      <div className="container">
+        <form action="#">
+          <div>
+            <label htmlFor="cardNumber">Card Number :</label>
+            <input
+              onChange={this.handleValue}
+              className="form-control"
+              type="number"
+              name="cardNumber"
+              id="cardNumber"
+              value={cardNumber}
+              ref={this.cardNumberRef}
+            />
           </div>
-          <div className="contact-form">
-            <form action="#">
-              <div>
-                <label htmlFor="email">Email</label>
-                <input
-                  value={email}
-                  onChange={this.handleEmail}
-                  type="email"
-                  id="email"
-                  name="email"
-                />
-              </div>
-              <div>
-                <input
-                  checked={isAgree}
-                  onChange={this.handleChecking}
-                  type="checkbox"
-                  id="agreement"
-                  name="agreement"
-                />
-                <label htmlFor="agreement">Terms &amp; Conditions</label>
-              </div>
-
-              <button onClick={this.submitBtn} className="btn" type="button">
-                - Submit
-              </button>
-            </form>
+          <div>
+            <label htmlFor="cardCVV">Card CVV :</label>
+            <input
+              onChange={this.handleValue}
+              className="form-control"
+              type="number"
+              name="cardCvv"
+              id="cardCvv"
+              ref={this.cardCvvRef}
+              value={cardCvv}
+            />
           </div>
-        </div>
+          <div>
+            <label htmlFor="cardName">Card Name :</label>
+            <input
+              onChange={this.handleValue}
+              className="form-control"
+              type="text"
+              name="cardName"
+              ref={this.cardNameRef}
+              id="cardName"
+              value={cardName}
+            />
+          </div>
+          <button className="btn btn-success m-2" type="submit">
+            Pay
+          </button>
+        </form>
       </div>
     );
   }
 }
 
 export default App;
+
+/* Old version  --->   */
+
+// import React, { Component } from "react";
+
+// export class App extends Component {
+
+//   componentDidMount() {
+//     this.refs.firstNameInput.focus()
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <form action="#">
+//           <div>
+//             <label htmlFor="firstName">First Name :</label>
+//             <input
+//               className="form-control"
+//               type="text"
+//               name="firstName"
+//               id="firstName"
+//               ref="firstNameInput"
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
